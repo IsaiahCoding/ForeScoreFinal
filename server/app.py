03a3536abc117bb1692ea01c9a75f84f2cfe46b1
Isaiah <IaguileraCFE@gmail.com>
2024-03-15 11:30:57 -0700

    PastRound and PastRound/id had functioning CRUD

--- server/app.py
+++ server/app.py
@@ -69,12 +69,66 @@ def scorecard_id(id):
         return make_response(response_body, 404)
     
 
-@app.route('/past_round', methods = ['GET'])
+@app.route('/past_round', methods=['GET', 'POST'])
 def past_round():
-    past_rounds = PastRound.query.all()
-    past_rounds_dict = [past_round.to_dict(rules = ('-user', )) for past_round in past_rounds]
-    response = make_response(jsonify(past_rounds_dict), 200)
+    if request.method == 'GET':
+        past_rounds = PastRound.query.all()
+        past_rounds_dict = [past_round.to_dict(rules=('-user',)) for past_round in past_rounds]
+        response = make_response(jsonify(past_rounds_dict), 200)
+    elif request.method == 'POST':
+        request_body = request.get_json()
+        new_past_round = PastRound(
+            date=request_body['date'],
+            course=request_body['course'],
+            par=request_body['par'],
+            score=request_body['score'],
+            fairway_hit=request_body['fairway_hit'],
+            green_in_regulation=request_body['green_in_regulation'],
+            putts=request_body['putts'],
+            total_score=request_body['total_score'],
+            user_id=request_body['user_id']
+        )
+        db.session.add(new_past_round)
+        db.session.commit()
+        response = make_response(jsonify(new_past_round.to_dict(rules=('-user',))), 201)
+    else:
+        response = make_response(
+            jsonify({"error": "Invalid request method"}),
+            400
+        )
     return response
+
+@app.route('/past_round/<int:id>', methods = ['GET', 'PATCH', 'POST', 'DELETE'])
+def past_round_id(id):
+    past_round = PastRound.query.get(id)
+    if past_round:
+        if request.method == 'GET':
+            response_body = jsonify(past_round.to_dict(rules = ('-user', )))
+            return make_response(response_body, 200)
+        elif request.method == 'PATCH':
+            request_body = request.get_json()
+            past_round.date = request_body['date']
+            past_round.course = request_body['course']
+            past_round.par = request_body['par']
+            past_round.score = request_body['score']
+            past_round.fairway_hit = request_body['fairway_hit']
+            past_round.green_in_regulation = request_body['green_in_regulation']
+            past_round.putts = request_body['putts']
+            past_round.total_score = request_body['total_score']
+            db.session.commit()
+            response_body = jsonify(past_round.to_dict(rules = ('-user', )))
+            return make_response(response_body, 200)
+        elif request.method == 'DELETE':
+            db.session.delete(past_round)
+            db.session.commit()
+            response_body = jsonify({"message": "Successfully deleted past round"})
+            return make_response(response_body, 200)
+        else:
+            response_body = jsonify({"error": "Invalid request method"})
+            return make_response(response_body, 400)
+    else:
+        response_body = jsonify({"error": "Past round not found"})
+        return make_response(response_body, 404)
 
 @app.route('/club', methods = ['GET'])
 def club():
