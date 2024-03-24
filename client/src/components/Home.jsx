98d1f0f3f87a9acb2a9c582b7c927288459971ea
Isaiah <IaguileraCFE@gmail.com>
2024-03-23 18:56:53 -0700

    updated Home comp. no more double NavBar rendering

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,6 @@ import NavBar from './NavBar';
 function Home() {
   return (
     <div>
-      <NavBar />
     
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-green-400 to-blue-500">
       <h1 className="text-4xl font-bold text-white mb-2">Welcome to Fore Score</h1>
